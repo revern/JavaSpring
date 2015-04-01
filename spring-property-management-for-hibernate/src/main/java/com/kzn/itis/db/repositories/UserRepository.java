5bc5af1ac602a17fa64c5dc254ae0b6823544897
revern <iceqrevern@gmail.com>
2015-04-01 23:22:26 +0300

    spring 1

---
+++ spring-property-management-for-hibernate/src/main/java/com/kzn/itis/db/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.kzn.itis.db.repositories;
+
+import com.kzn.itis.db.model.User;
+
+public interface UserRepository {
+
+    public void addUser(User user);
+    public void update(int id, String firstname, String secondname, int age);
+    public void delete(int id);
+    public void showAll();
+    public long getCount();
+}
